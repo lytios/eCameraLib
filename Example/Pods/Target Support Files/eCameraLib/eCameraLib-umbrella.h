f289dfc1d0d1b007c4aea8f72665b8d9ae9665d1
lyt <24290265@qq.com>
2019-07-26 17:17:40 +0800

    提交

---
+++ Example/Pods/Target Support Files/eCameraLib/eCameraLib-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "eTakeCameraViewController.h"
+
+FOUNDATION_EXPORT double eCameraLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char eCameraLibVersionString[];
+
